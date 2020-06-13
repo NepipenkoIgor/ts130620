962696a5418f20918b112cdb57599b701a859c11
Ihor <igornepipenko@gmail.com>
2020-06-13 14:19:05 +0300

    Feature: features-v2 [ref:no-ref]

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,21 @@
-/** let/const */
+function sleep(ms: number) {
+    return new Promise((res) => {
+        setTimeout(res, ms)
+    })
+}
 
-let period = 60;
-const baseUrl = 'http://localhost:4200';
-
-/** object */
-let firstName = 'Igor';
-
-let account = {
-    firstName,
-    getName() {
-        return this.firstName;
+async function* getItemsReallySlowly<T>(items: Iterable<T>): AsyncIterableIterator<T> {
+    for (const item of items) {
+        await sleep(1000);
+        yield  item;
     }
 }
 
-/** .... */
 
-let person = {...account};
-let dates = [...[11, 12, 15]];
-
-
-/** dustructoring */
-
-let {firstName: username} = account;
-let [firstDate] = dates;
-
-/** template str**/
-function userMessage([start, end]: TemplateStringsArray, {firstName: name}: typeof person) {
-    return `${start}${name}${end}`
-}
-
-console.log(userMessage`Good day, ${person} !!!`);
-
-
-// let str = '5';
-// let n: number  = str as any as number;
-
-
-/** for of**/
-for (const date of dates) {
-    console.log(date);
-}
-
-/** arrow fn */
-
-let sum = (a: number, b: number) => a + b;
-
-/** class */
-class Point {
-    private x: number = 10;
-
-    getX() {
-        return this.x;
+async function speakLikeSloth(items: string[]) {
+    for await  (const item of getItemsReallySlowly(items)) {
+        console.log(item);
     }
 }
 
-/**  Optional chaining */
-let user: any = {};
-let prop = user?.info?.getSalary();
-
-/*** ?? */
-let width = 0;
-let w = width ?? 10;
-
-/** includes */
-let a = [1, 2, 3].includes(2);
+speakLikeSloth(['hi', 'all', 'TypeScript', 'is', 'awesome', '!']);
