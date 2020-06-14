2191833e02efb5d4ef6433c5e8e2884f9bc13a4f
Ihor <igornepipenko@gmail.com>
2020-06-14 13:32:38 +0300

    Feature: menu jquery  [ref:no-ref]

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,19 @@ import { generateMenu } from './menu';
 import { list } from './menu/data';
 
 
-const navMenu: HTMLDivElement = document.querySelector('.menu') as HTMLDivElement;
-navMenu.innerHTML = generateMenu(list);
-navMenu.addEventListener('click', (event: MouseEvent) => {
-    const el: HTMLElement = event.target as HTMLElement;
-    if (!el.classList.contains('title')) {
-        return;
-    }
-    const parentLi: HTMLLIElement = el.parentElement as HTMLLIElement;
-    parentLi.classList.toggle('menu-open')
-})
+$('.menu')
+    .html(generateMenu(list))
+    .on('click', (e: Event) => {
+        const el: HTMLElement = e.target as HTMLElement;
+        if (!el.classList.contains('title')) {
+            return;
+        }
+        const parentLi: HTMLLIElement = el.parentElement as HTMLLIElement;
+        parentLi.classList.toggle('menu-open')
+    });
+
+// const navMenu: HTMLDivElement = document.querySelector('.menu') as HTMLDivElement;
+// navMenu.innerHTML = generateMenu(list);
+// navMenu.addEventListener('click', (event: MouseEvent) => {
+//
+// })
